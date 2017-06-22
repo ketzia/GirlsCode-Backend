a921a0a2f3d0b1a23d58bb40c4264217be457a5e
Ketzia Dante <ketzia.dante@gmail.com>
2017-06-21 20:42:35 -0500

    adding access control allow origin

--- server.js
+++ server.js
@@ -11,7 +11,24 @@ const port = process.env.PORT || 8080;
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 mongoose.connect('mongodb://localhost/test');
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 const api_routes = require('./app/index');
 api_routes(app);
 
